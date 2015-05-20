86cae1c1bc2b5a8a71443190ed24ca9783a5276c
Vivek <vivekaxl@gmail.com>
2015-05-20 17:54:36 -0400

    convergence not working

--- Convergence/main.py
+++ Convergence/main.py
@@ -1,10 +1,11 @@
 from __future__ import division
 from individual import individual
 from random import random, choice, uniform
+from math import fabs
 
 
 MIN = 1
-MAX = 100
+MAX = 1000
 def generate_population(number = 100):
     assert(number > 0), "Population needs to be greater than 0"
     population = {}
@@ -21,7 +22,7 @@ def euclidean_distance(list1, list2):
 
 
 # approximation
-def convex_hull(individual, population, radius = 0.1 * MAX):
+def convex_hull(individual, population, radius = 0.05 * MAX):
     origin = individual.decision
     neighbourhood = []
     for key in population.keys():
@@ -30,53 +31,49 @@ def convex_hull(individual, population, radius = 0.1 * MAX):
 
     for key in population.keys():
         pop = population[key]
-        if pop.distance <= radius:
+        if pop.distance <= radius and pop.id != individual.id:
+            # print pop.distance, radius
             neighbourhood.append(pop)
-    print "Length: ", len(neighbourhood)
+    # print "Length: ", len(neighbourhood)
     return neighbourhood
 
 
-def push_neighbor(pushed, pole, nudge_ratio = 0.4):
+def push_neighbor(pushed, pole, nudge_ratio = 0.05):
     assert(len(pushed.decision) == len(pole.decision)), "Something is wrong"
     # 10% push towards pole - neighbour
     # rand() push towards pole - center
-    nudge = euclidean_distance(pushed.decision, pole.decision) * nudge_ratio
+    # print "B: ", pushed.decision
     for i, p in enumerate(pushed.decision):
-        pushed.decision[i] = max(0, min(pushed.decision[i] + nudge, MAX))
+        pushed.decision[i] = abs((pushed.decision[i] + pole.decision[i]) * (1 - nudge_ratio))%MAX
+    # print "A: ", pushed.decision
     return pushed
 
 
 import matplotlib.pyplot as plt
 def main_loop():
     population = generate_population()
-    count = 0
-
     import pylab
     pylab.ion()       # Turn on interactive mode.
     pylab.hold(False) # Clear the plot before adding new data.
-
+    count = 0
     while True:
         count += 1
         for key in population.keys():
             pop = population[key]
             neighborhood = convex_hull(pop, population)
             neighborhood = [push_neighbor(neigh, pop) for neigh in neighborhood]
+            if len(neighborhood) == 0: continue
             random_direction = choice(neighborhood)
             population[pop.id] = push_neighbor(pop, random_direction, 0.1 + random())
             for neigh in neighborhood:
-                # print "Decision: ", neigh.decision
                 population[neigh.id] = (neigh)
-        # raw_input()
-        if count % 10 == 0:
-
+        if count % 5 == 0:
             x = []
             y = []
             for key in population.keys():
                 pop = population[key].decision
                 x.append(pop[0])
                 y.append(pop[1])
-            # print len(x), len(y)
-            # print len(set(x)), len(set(y))
             pylab.scatter(x, y)
             pylab.draw()
             print "There"
