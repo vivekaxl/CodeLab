80004e16a62ed28edf42769f902ffb97c8a0c184
Vivek Nair <vivekaxl@gmail.com>
2016-01-11 13:30:52 -0500

    heat maps

--- HeatMaps/script.py
+++ HeatMaps/script.py
@@ -46,6 +46,7 @@ def euclidean_distance(list1, list2):
     assert(distance >= 0), "Distance can't be less than 0"
     return distance
 
+
 def build_similarity_matrix(data):
     """
     higher the value more different the points are
@@ -55,16 +56,64 @@ def build_similarity_matrix(data):
     similarity_matrix = [[euclidean_distance(data[i], data[j]) if i!= j else 0 for j in xrange(dimension)] for i in xrange(dimension)]
     return similarity_matrix
 
-def method2(data):
-    """similar solutions together: based on euclidean distance"""
-    similarity_matrix = build_similarity_matrix(data)
-
-
-
 
 def method2(data):
+    # Generate random features and distance matrix.
+    import scipy
+    import pylab
+    import scipy.cluster.hierarchy as sch
+    x = scipy.array(xrange(len(data)))
+    D = scipy.array(build_similarity_matrix(data))
+
+    # Compute and plot first dendrogram.
+    fig = pylab.figure(figsize=(15,15))
+    ax1 = fig.add_axes([0.09,0.1,0.2,0.6])
+    Y = sch.linkage(D, method='centroid')
+    Z1 = sch.dendrogram(Y, orientation='right')
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+
+    # Compute and plot second dendrogram.
+    ax2 = fig.add_axes([0.3,0.71,0.6,0.2])
+    Y = sch.linkage(D, method='single')
+    Z2 = sch.dendrogram(Y)
+    ax2.set_xticks([])
+    ax2.set_yticks([])
+
+    # Plot distance matrix.
+    axmatrix = fig.add_axes([0.3,0.1,0.6,0.6])
+    idx1 = Z1['leaves']
+    idx2 = Z2['leaves']
+    D = D[idx1,:]
+    D = D[:,idx2]
+    im = axmatrix.matshow(D, aspect='auto', origin='lower', cmap=pylab.cm.YlGnBu)
+    axmatrix.set_xticks([])
+    axmatrix.set_yticks([])
+
+    axmatrix.set_xticks(range(len(data)))
+    axmatrix.set_xticklabels(idx1, minor=False)
+    axmatrix.xaxis.set_label_position('bottom')
+    axmatrix.xaxis.tick_bottom()
+
+    pylab.xticks(rotation=-90, fontsize=8)
+
+    axmatrix.set_yticks(range(len(data)))
+    axmatrix.set_yticklabels(idx2, minor=False)
+    axmatrix.yaxis.set_label_position('right')
+    axmatrix.yaxis.tick_right()
+    pylab.yticks( fontsize=8)
+
+    axcolor = fig.add_axes([0.94,0.1,0.02,0.6])
+    pylab.colorbar(im, cax=axcolor)
+    fig.show()
+    fig.savefig('./Chart/dendrogram.png')
+
+    # This is not working completely. But you can draw inspiration
+
+
 
 
 if __name__ == "__main__":
     data = read_data()
     method1(data)
+    # method2(data)
